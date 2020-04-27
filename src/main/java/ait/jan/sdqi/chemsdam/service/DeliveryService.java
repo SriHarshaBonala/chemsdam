b25702b44248d2268d4a456f0cc677a004ef8040
SriHarshaBonala <64273763+SriHarshaBonala@users.noreply.github.com>
2020-04-27 09:32:10 +0700

    Updated File

--- src/main/java/ait/jan/sdqi/chemsdam/service/DeliveryService.java
+++ src/main/java/ait/jan/sdqi/chemsdam/service/DeliveryService.java
@@ -1,6 +1,5 @@
 package ait.jan.sdqi.chemsdam.service;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
@@ -13,4 +12,5 @@ public interface DeliveryService
 	void register(Delivery delivery);
 	Delivery validate(HttpServletRequest req, Delivery delivery);
 	void update_details(Delivery delivery);
+	void delete_delivery(Delivery delivery, String delivery_reference_number);
 }
