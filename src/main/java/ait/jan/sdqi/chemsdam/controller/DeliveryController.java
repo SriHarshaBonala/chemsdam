23356d488891b9f935794de67fe32e7e0afb3167
SriHarshaBonala <64273763+SriHarshaBonala@users.noreply.github.com>
2020-04-27 09:23:05 +0700

    Updated File

--- src/main/java/ait/jan/sdqi/chemsdam/controller/DeliveryController.java
+++ src/main/java/ait/jan/sdqi/chemsdam/controller/DeliveryController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ait.jan.sdqi.chemsdam.model.Delivery;
@@ -17,6 +18,7 @@ import ait.jan.sdqi.chemsdam.service.DeliveryServiceImpl;
 @Controller
 public class DeliveryController 
 {
+	//@Autowired Delivery delivery;
 	@Autowired public DeliveryService service;
 	@Autowired public DeliveryServiceImpl serv_impl;
 	
@@ -28,10 +30,40 @@ public class DeliveryController
 		return model;
 	}
 	
+	@RequestMapping(value = "/future_delivery")
+	public ModelAndView showFutureDeliveries(HttpServletRequest request, HttpServletResponse response)
+	{
+		ModelAndView model = new ModelAndView("future_delivery");
+		model.addObject("future_deliveries", new Delivery());
+		return model;
+	}
+	
 	@RequestMapping(value = "register_delivery", method = RequestMethod.POST)
 	public String addDelivery(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("registerDelivery") Delivery delivery)
 	{
 		service.register(delivery);
+		return "redirect:/dms_home.jsp";
+	}
+	
+	@RequestMapping(value = "edit_delivery")
+	public ModelAndView editDelivery(@RequestParam String delivery_reference_number)
+	{
+		ModelAndView model = new ModelAndView("edit_delivery");
+		model.addObject("delivery_reference_number", delivery_reference_number);
+		return model;
+	}
+	
+	@RequestMapping(value = "update_delivery", method = RequestMethod.POST)
+	public String updateDelivery(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("editDelivery") Delivery delivery)
+	{
+		service.update_details(delivery);
+		return "redirect:/future_delivery.jsp";
+	}
+	
+	@RequestMapping(value = "delete_delivery")
+	public String deleteDelivery(Delivery delivery, @RequestParam String delivery_reference_number)
+	{
+		service.delete_delivery(delivery, delivery_reference_number);
 		return "redirect:/future_delivery.jsp";
 	}
 }
